ac532d1412d7104d753d7bcd1be8cac58aff50d8
Anton Butovich <abutovich@gmail.com>
2014-06-26 23:02:40 +0300

    fix test port variable

--- test/test.py
+++ test/test.py
@@ -45,9 +45,8 @@ class JenkinsDevComponentTestCase(BaseComponentTestCase):
         "name": name,
         "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name))
     }]
-    port = "8080"
     @instance(byApplication=name)
-    @values({"output.jenkins-server-host": "hosts"})
+    @values({"output.jenkins-server-host": "hosts", "8080":"port"})
     def test_port(self, instance, hosts, port):
         import socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
