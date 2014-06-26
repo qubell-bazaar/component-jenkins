2246110feed888ced33faac36dbc193c129b33c8
Anton Butovich <abutovich@gmail.com>
2014-06-26 18:17:36 +0300

    setup tests

---
+++ Berksfile
@@ -0,0 +1,8 @@
+site :opscode
+
+cookbook "apt", ref: "1.1.1"
+cookbook "java", ref: "1.5.2"
+cookbook "openssl", git: 'https://github.com/opscode-cookbooks/openssl.git', ref: "v1.1.0"
+cookbook 'chef-solo-search', github: 'edelight/chef-solo-search', ref: '0.4.0'
+cookbook "build-essential", git: 'https://github.com/opscode-cookbooks/build-essential.git', ref: "v1.4.4"
+cookbook "cookbook_qubell_jenkins", git: 'https://github.com/jollyrojer/cookbook_qubell_jenkins.git'
---
+++ test/test.py
@@ -0,0 +1,57 @@
+import os
+
+from test_runner import BaseComponentTestCase
+from qubell.api.private.testing import instance, environment, workflow, values
+
+@environment({
+    "default": {},
+    "AmazonEC2_CentOS_63": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-eb6b0182"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "root"
+        }]
+    },
+    "AmazonEC2_CentOS_53": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-beda31d7"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "root"
+        }]
+    },
+    "AmazonEC2_Ubuntu_1204": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-d0f89fb9"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "ubuntu"
+        }]
+    }
+})
+class JenkinsDevComponentTestCase(BaseComponentTestCase):
+    name = "component-jenkins"
+    apps = [{
+        "name": name,
+        "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name))
+    }]
+
+    @instance(byApplication=name)
+    @values({"output.jenkins-server-host": "hosts"})
+    def test_port(self, instance, hosts, port):
+        import socket
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((hosts, int(8080)))
+
+        assert result == 0
